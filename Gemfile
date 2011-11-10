616828ac3e9db56bd48cc0e1922365d28d1e31d8
Patrick Breucking <me@patrickbreucking.de>
2011-11-10 22:58:54 +0100

    Added oauth to gems Added some stuff to ning.rb, trying to port the Ning Python API

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ end
 gem 'jquery-rails'
 gem 'therubyracer'
 
+gem 'oauth'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/ning.rb
+++ app/models/ning.rb
@@ -1,6 +1,72 @@
 class Ning
+  
+  attr_accessor :host, :network, :consumer, :client
+  
   def initialize(apikey)
     # connect to NING API
+    @consumer=OAuth::Consumer.new( "0d7fc228-3365-4b89-94aa-b23ba30a8cef","64005976-973c-4725-92ec-b5493cd8ab9a
+", {
+        :site=>"external.ningapis.com"
+    })
+    
+    @host = "external.ningapis.com"
+    @network = "foo"
+    
+    
+    email = "test@example.com"
+    password = "I<3Ning"
+    
+    self.login(email, password)
+    
+  end
+  
+  def login(login, password)
+    self.call("Token", method="Post",headers={
+         #'Authorization': 'Basic %s' %
+        #  binascii.b2a_base64('%s:%s' % (login, password)),
+       }, secure=true)
+  end
+  
+  def call(url, method="GET", body='', token=nil, headers=nil,
+        secure=false)
+        ###
+        ### Code copied from Ning Python API, try to port to ruby
+        ###
+        
+        
+        #if self.method.name == 'PLAINTEXT':
+        #    secure = true
+        #end
+        #if secure:
+        #    protocol = self.SECURE_PROTOCOL
+        #else:
+        #    protocol = self.INSECURE_PROTOCOL
+
+        #url = '%s%s/xn/rest/%s/1.0/%s' % (protocol, self.host, self.network, url)
+        #self.client = oauth.Client(self.consumer, token)
+        #if self.method is not None:
+        #    self.client.set_signature_method(self.method)
+
+        #resp, content = self.client.request(url, method, headers=headers,
+        #    body=body)
+        #if int(resp['status']) != 200:
+        #    try:
+        #        result = json.loads(content)
+        #        if result:
+        #            raise NingError(result['status'], result['reason'],
+        #                result['code'], result['subcode'], result['trace'])
+         #       else:
+        #            raise NingError(int(resp['status']),
+        #                "HTTP response %s and %s" % (resp, content))
+        #    except ValueError:
+        #        raise NingError(int(resp['status']),
+        #            "HTTP response %s and %s" % (resp, content))#
+
+        #return json.loads(content)
+  end
+  
+  def post(path, body)
+    
   end
 
   def users
