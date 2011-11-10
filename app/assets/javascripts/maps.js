deb60ccd4129e3277100839f348e0c70f993db70
Patrick Breucking <me@patrickbreucking.de>
2011-11-10 22:20:27 +0100

    Refactored Javascript Code Mode JS Code to correct place Added a Button which loads Users from JSON into the map Added a onClick Listener to the markers to show some information

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,70 @@
+
+var map;
+var geocoder = new google.maps.Geocoder();
+
+function codeAddress(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                title: "FerUniversität",
+                position: results[0].geometry.location
+            });
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
+
+function loadUsers() {
+
+    $.ajaxSetup ({  
+        cache: false  
+    });
+    var loadUrl = "/?format=json";  
+
+    $.getJSON(loadUrl, function(data) {
+        var items = [];
+
+        $.each(data, function(key, val) {
+            console.info(val.location);
+            geocoder.geocode( { 'address': val.location}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        draggable: false,
+                        title: val.name,
+                        animation: google.maps.Animation.DROP,
+                        position: results[0].geometry.location
+                    });
+                    var infowindow = new google.maps.InfoWindow({
+                        content: "<div>Hier wohnt Benutzer: "+val.name+"</div>"
+                    });
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(map,marker);
+                     });
+                     
+                } else {
+                    alert("Geocode was not successful for the following reason: " + status);
+                }
+           });
+           
+        });
+    });
+}
+
+
+function initialize() {
+
+    var address = "Universitätsstr. 1, Hagen, Germany";
+    codeAddress(address);
+    var myOptions = {
+        zoom: 4,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    };
+    map = new google.maps.Map(document.getElementById('map_canvas'),
+    myOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
