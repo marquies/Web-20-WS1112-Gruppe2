d02a77413e37bf046704e6b799f1befdcb4e4752
Christoph Thiel <ct@kki.org>
2011-11-10 21:55:18 +0100

    provide users as json or xml

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
   def index
     @users = Ning.new('testkey').users
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @users.to_xml }
+      format.json { render :json => @users.to_json }
+    end
   end
 
 end
